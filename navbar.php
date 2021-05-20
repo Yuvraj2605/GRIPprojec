1658a5317dd09428ae1c6b8afe4660ad0b8aca8a
Yuvraj2605 <84504281+Yuvraj2605@users.noreply.github.com>
2021-05-20 02:57:19 -0700

    first commit

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color :Black;"><b>GRIP BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html" style="color :Black ;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="NewUser.php" style="color :Black;"><b>New User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color :Black;"><b>Make a Transaction</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color :Black;"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
